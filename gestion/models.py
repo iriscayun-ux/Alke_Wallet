384fb6984494d8ea16a24c45459f9ef9c7a495c3
iriscayun <iriscayun@gmail.com>
2026-04-09 14:50:25 -0400

    Agregado estado de transacciones y conexión MySQL

--- gestion/models.py
+++ gestion/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#  CLIENTE
+# CLIENTE
 class Cliente(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     telefono = models.CharField(max_length=15)
@@ -11,17 +11,17 @@ class Cliente(models.Model):
         return self.usuario.username
 
 
-#  CUENTA
+# CUENTA
 class Cuenta(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Cuenta {self.id} - {self.cliente}"
+        return f"Cuenta {self.id}"
 
 
-#  TRANSACCIÓN
+# TRANSACCION
 class Transaccion(models.Model):
     TIPO = [
         ('deposito', 'Depósito'),
@@ -33,13 +33,11 @@ class Transaccion(models.Model):
         ('fallo', 'Fallida'),
     ]
 
-    # Relación con Cuenta
-    
     cuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE)
     tipo = models.CharField(max_length=10, choices=TIPO)
     monto = models.DecimalField(max_digits=10, decimal_places=2)
     fecha = models.DateTimeField(auto_now_add=True)
-    estado = models.CharField(max_length=10, choices=ESTADO, default='exito')  # ✅ nuevo campo
+    estado = models.CharField(max_length=10, choices=ESTADO, default='exito')
 
     def __str__(self):
         return f"{self.tipo} - {self.monto} - {self.estado}"
